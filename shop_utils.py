883a3afda138d60933b734045f2a2bdacef1f787
Anton Pozdnyakov <Anton.Pozdnyakov.I@gmail.com>
2021-12-02 12:01:04 +0300

    Add shop utils

---
+++ shop_utils.py
@@ -0,0 +1,238 @@
+import json
+import os
+import requests
+
+from dotenv import load_dotenv
+from funcy import retry
+
+load_dotenv()
+API_BASE_URL = os.environ.get('API_BASE_URL', 'https://api.moltin.com')
+CLIENT_ID = os.environ["CLIENT_ID"]
+CLIENT_SECRET = os.environ["CLIENT_SECRET"]
+
+
+@retry(tries=3, timeout=1)
+def add_product_to_cart(cart_id, product_id, quantity):
+    """
+    Добавляет товар в корзину
+    :param cart_id: ID корзины
+    :param product_id: ID товара
+    :param quantity: Количество товара
+    :return: Результат (в т.ч. ошибку) как JSON объект
+    """
+    headers = {
+        'Authorization': f'Bearer {get_actual_token()}',
+        'Content-Type': 'application/json',
+    }
+    data = {"data":
+        {
+            "id": product_id,
+            "type": "cart_item",
+            "quantity": quantity
+        }
+    }
+    response = requests.post(
+        f'{API_BASE_URL}/v2/carts/{cart_id}/items',
+        headers=headers,
+        data=json.dumps(data)
+    )
+    response.raise_for_status()
+    return response.json()
+
+
+@retry(tries=3, timeout=1)
+def create_a_file(folder_name='images'):
+    """
+    Загружает файлы в систему CMS.
+    Проверяет папку (по умолчанию 'images') и загружает все найденные картинки.
+    Загруженные картинки переименовывает в имя_файла.расширение.uploaded
+    Возвращает количество загруженных картинок и их список
+    """
+    headers = {'Authorization': f'Bearer {get_actual_token()}'}
+
+    filenames = os.listdir(folder_name)
+    uploaded_files = []
+    for filename in filenames:
+        if 'uploaded' in filename:
+            continue
+
+        filename_path = os.path.join(folder_name, filename)
+        files = {
+            'file': (filename, open(filename_path, 'rb')),
+            'public': (None, 'true'),
+        }
+        response = requests.post(
+            f'{API_BASE_URL}/v2/files',
+            headers=headers,
+            files=files
+        )
+        response.raise_for_status()
+
+        uploaded_files.append(filename)
+        uploaded_filename_path = os.path.join(
+            folder_name,
+            f'{filename}.uploaded'
+        )
+        os.rename(filename_path, uploaded_filename_path)
+
+    return f'Uploaded {len(uploaded_files)} files. Details: {uploaded_files}'
+
+
+@retry(tries=3, timeout=1)
+def create_a_customer(name, email):
+    """
+    Создает покупателя.
+    Поле пароля не предусмотрено
+    :param name: Имя покупателя
+    :param email: Email покупателя
+    :return: Результат (в т.ч. ошибку) как JSON объект
+    """
+
+    headers = {
+        'Authorization': f'Bearer {get_actual_token()}',
+        'Content-Type': 'application/json',
+    }
+    data = {"data":
+        {
+            "type": "customer",
+            "name": name,
+            "email": email
+        }
+    }
+    response = requests.post(
+        f'{API_BASE_URL}/v2/customers',
+        headers=headers,
+        data=json.dumps(data)
+    )
+    response.raise_for_status()
+    return response.json()
+
+
+@retry(tries=3, timeout=1)
+def create_main_image_relationship(product_id, image_id):
+    """
+    Привязывает главную картинку для продукта на основании ID продукта
+     и ID картинки.
+    """
+    headers = {
+        'Authorization': f'Bearer {get_actual_token()}',
+        'Content-Type': 'application/json',
+    }
+
+    data = {"data": {"type": "main_image", "id": image_id}}
+
+    response = requests.post(
+        f'{API_BASE_URL}/v2/products/{product_id}/relationships/main-image',
+        headers=headers,
+        data=json.dumps(data)
+    )
+    response.raise_for_status()
+    return response.json()
+
+
+@retry(tries=3, timeout=1)
+def get_actual_token():
+    """
+    Возвращает актуальный токен, т.к. токены имеют свойство _протухать_
+    """
+    data = {
+        'client_id': CLIENT_ID,
+        'grant_type': 'client_credentials',
+        'client_secret': CLIENT_SECRET,
+    }
+    response = requests.post(
+        f'{API_BASE_URL}/oauth/access_token',
+        data=data
+    )
+    response.raise_for_status()
+    return response.json()['access_token']
+
+
+@retry(tries=3, timeout=1)
+def get_a_customers(customer_id=None):
+    """Возвращает список всех покупателей или конкретного покупателя по его ID"""
+
+    headers = {'Authorization': f'Bearer {get_actual_token()}'}
+    url = f'{API_BASE_URL}/v2/customers/{customer_id}' if customer_id else f'{API_BASE_URL}/v2/customers'
+
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    return response.json()
+
+
+@retry(tries=3, timeout=1)
+def get_files(file_id=None):
+    """
+    Возвращает описание всех загруженных файлов или конкретного файла по его ID
+    """
+    headers = {'Authorization': f'Bearer {get_actual_token()}'}
+
+    url = f'{API_BASE_URL}/v2/files/'
+    if file_id:
+        url += file_id
+
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    return response.json()
+
+
+@retry(tries=3, timeout=1)
+def get_cart_status(card_id, items=False):
+    """
+    Возвращает статус корзины или ее список товаров в ней
+    """
+    headers = {'Authorization': f'Bearer {get_actual_token()}'}
+
+    url = f'{API_BASE_URL}/v2/carts/{card_id}'
+    if items:
+        url += '/items'
+
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    return response.json()
+
+
+@retry(tries=3, timeout=1)
+def get_products(product_id=None):
+    """
+    Возвращает описание всех продуктов или описание конкретного продукта по его ID
+    """
+
+    headers = {'Authorization': f'Bearer {get_actual_token()}'}
+
+    url = f'{API_BASE_URL}/v2/products/'
+    if product_id:
+        url += product_id
+
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    return response.json()
+
+
+@retry(tries=3, timeout=1)
+def remove_item_from_cart(card_id, product_id):
+    """
+    Удаляет товар из конкретной корзины (cart_id) по ID-товара
+    """
+    headers = {'Authorization': f'Bearer {get_actual_token()}'}
+
+    url = f'{API_BASE_URL}/v2/carts/{card_id}/items/{product_id}'
+
+    response = requests.delete(url, headers=headers)
+    response.raise_for_status()
+
+    return response.json()
+
+
+if __name__ == '__main__':
+    """
+    Здесь формируем запросы
+    """
+    # example = get_products()
+    # print(example)
+
+
